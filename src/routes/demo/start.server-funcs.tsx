131d7fa001a81b44ed22bd1b384f18eef8b39c27
Stefan Kracht <stefankracht@gmail.com>
2026-02-09 06:58:23 +0100

    fix ts issues

--- src/routes/demo/start.server-funcs.tsx
+++ src/routes/demo/start.server-funcs.tsx
@@ -73,7 +73,7 @@ function Home() {
       <div className="w-full max-w-2xl rounded-xl border-8 border-black/10 bg-black/50 p-8 shadow-xl backdrop-blur-md">
         <h1 className="mb-4 text-2xl">Start Server Functions - Todo Example</h1>
         <ul className="mb-4 space-y-2">
-          {todos?.map((t) => (
+          {todos?.map((t: { id: number; name: string }) => (
             <li
               className="rounded-lg border border-white/20 bg-white/10 p-3 shadow-md backdrop-blur-sm"
               key={t.id}
