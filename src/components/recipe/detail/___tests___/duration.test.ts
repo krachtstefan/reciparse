7622acab389f22662c2d525e21f643e12364471d
Stefan Kracht <stefankracht@gmail.com>
2026-02-20 15:28:55 +0100

    move tests

--- src/components/recipe/detail/___tests___/duration.test.ts
+++ src/components/recipe/detail/___tests___/duration.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { formatDuration } from "./duration";
+import { formatDuration } from "../recipe-detail/duration";
 
 describe("formatDuration", () => {
   it("formats minutes", () => {
